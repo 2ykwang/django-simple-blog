b1cb34414c13051634963fc4a764e6e3e07de551
Youngkwang Yang <immutable000@gmail.com>
2022-06-06 06:01:50 +0900

    카테고리 기능 구현 (#3)
    * 카테고리 템플릿 파일 작성
    
    * 카테고리 모델 추가및 기능 구현

--- apps/posts/models/post.py
+++ apps/posts/models/post.py
@@ -1,9 +1,22 @@
+from typing import Tuple
+
 from django.db import models
 from django.urls import reverse
+from django.utils.html import strip_tags
+from markdownx.utils import markdownify
 
 from apps.core.fields import UUIDImageField
 from apps.core.models import TimeStampedModel
 from apps.posts.apps import PostsConfig as Config
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super(PublishedManager, self)
+            .get_queryset()
+            .filter(status__in=Post.public_status())
+        )
 
 
 class Post(TimeStampedModel):
@@ -11,10 +24,15 @@ class Post(TimeStampedModel):
         PUBLISHED = 0, "공개"
         DRAFT = 1, "초안"
         PRIVATE = 2, "비공개"
+        ONLY_LINK = 3, "일부 공개"
 
     class Meta:
         verbose_name = "게시글"
         verbose_name_plural = "게시글"
+        ordering = ["-created"]
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     thumbnail = UUIDImageField(
         verbose_name="썸네일", upload_to="uploads/", null=True, blank=True
@@ -30,8 +48,40 @@ class Post(TimeStampedModel):
         verbose_name="상태", choices=Status.choices, default=Status.DRAFT
     )
 
+    category = models.ForeignKey(
+        to="posts.category",
+        related_name="posts",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        if not self.description:
+            # description을 빈 값으로 저장할 경우 내용에서 가져온다.
+            # markdown - > html - > plain text 으로 변환 후 저장
+            self.description = strip_tags(markdownify(self.content))[:140]
+
+        super().save(force_insert, force_update, using, update_fields)
+
+    @property
+    def estimate_reading_time(self) -> int:
+        wpm = 100
+        word_count = len(self.content) // 5
+        return word_count // wpm
+
     def get_absolute_url(self):
         return reverse(f"{Config.name}:post_detail", kwargs={"slug": self.slug})
 
     def __str__(self):
         return self.title
+
+    @staticmethod
+    def public_on_category_status() -> Tuple:
+        return (Post.Status.PUBLISHED.value,)
+
+    @staticmethod
+    def public_status() -> Tuple:
+        return Post.Status.PUBLISHED.value, Post.Status.ONLY_LINK.value
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import DetailView, ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, ListView, TemplateView, View
 
-from .models import Post
+from .models import Category, Post
 
 
 class PostDetailView(DetailView):
@@ -8,8 +9,18 @@ class PostDetailView(DetailView):
 
 
 class PostListView(ListView):
-    model = Post
+    def get_queryset(self):
+        queryset = Post.objects.select_related("category")
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
         return context
+
+
+class CategoryPostListView(ListView):
+    template_name = "posts/post_list.html"
+
+    def get_queryset(self):
+        category = get_object_or_404(Category, slug=self.kwargs.get("slug"))
+        return category.get_posts()
