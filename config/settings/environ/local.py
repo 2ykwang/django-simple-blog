849413eb0e0c9fc4868c455379d4b620355528c5
Youngkwang Yang <immutable000@gmail.com>
2022-06-05 18:51:15 +0900

    django debug toolbar 라이브러리 추가

--- config/settings/environ/local.py
+++ config/settings/environ/local.py
@@ -2,8 +2,15 @@ from config.settings.base import *
 
 DEBUG = True
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 if ENABLE_SILK:
     INSTALLED_APPS += ["silk"]
     MIDDLEWARE += ["silk.middleware.SilkyMiddleware"]
+
+if ENABLE_DEBUG_TOOLBAR:
+    INSTALLED_APPS += ["debug_toolbar"]
+    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,15 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("apps.posts.urls")),
 ]
 
 if settings.ENABLE_SILK:
     urlpatterns += [
         path("silk/", include("silk.urls", namespace="silk")),
+    ]
+
+if settings.ENABLE_DEBUG_TOOLBAR:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
     ]
