deb144c23bd1c75d195b6e07bf91b0d60f27e7b3
Youngkwang Yang <immutable000@gmail.com>
2022-06-06 17:12:56 +0900

    사이트 내 meta description 추가 (#6)
    * site meta description 데이터 추가
    
    * gunicorn 의존성 추가
    
    * wsgi 기본 settings module 값 설정

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -15,12 +15,25 @@ class PostListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
+        context["list_title"] = "글 목록"
         return context
 
 
 class CategoryPostListView(ListView):
     template_name = "posts/post_list.html"
 
+    def _get_category(self):
+        # TODO: select category 두 번 발생
+        self.category = get_object_or_404(Category, slug=self.kwargs.get("slug"))
+        return self.category
+
     def get_queryset(self):
-        category = get_object_or_404(Category, slug=self.kwargs.get("slug"))
+        category = self._get_category()
         return category.get_posts()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        category = self._get_category()
+        context = super(CategoryPostListView, self).get_context_data()
+        context["category"] = category
+        context["list_title"] = f"{category.name} - 글 목록"
+        return context
