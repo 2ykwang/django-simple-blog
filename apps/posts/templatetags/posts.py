3a4a363726dbc9489fc2c9511fc4ba6c1d09320c
Youngkwang Yang <immutable000@gmail.com>
2022-06-13 00:03:44 +0900

    post 탬플릿 태그 정리

--- apps/posts/templatetags/posts.py
+++ apps/posts/templatetags/posts.py
@@ -1,4 +1,6 @@
 from django import template
+from django.utils.safestring import mark_safe
+from markdownx.utils import markdownify
 
 from apps.posts.models import Post
 
@@ -9,3 +11,8 @@ register = template.Library()
 def show_recent_posts():
     posts = Post.posts.public_on_category().all()[:5]
     return {"posts": posts}
+
+
+@register.filter
+def markdown(text: str):
+    return mark_safe(markdownify(text))
