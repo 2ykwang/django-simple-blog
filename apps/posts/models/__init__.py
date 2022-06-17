e9ab588d9f6ddd7205dd502d1a915f1aca94dc15
Youngkwang Yang <immutable000@gmail.com>
2022-06-18 01:37:29 +0900

    테스팅 환경 구성 및 테스트코드 작성 (#13)
    * 테스팅 의존성 추가
    
    * 커버리지 설정파일 `.coveragerc` 추가
    
    * post 모델 테스트 코드 추가

--- apps/posts/models/__init__.py
+++ apps/posts/models/__init__.py
@@ -1,3 +1,5 @@
 from .category import Category
 from .post import Post
 from .tag import Tag
+
+from .factory import PostFactory, CategoryFactory
---
+++ apps/posts/models/factory.py
@@ -0,0 +1,31 @@
+from factory.django import DjangoModelFactory
+
+from apps.posts.models import Post, Category
+import factory.fuzzy
+
+
+class CategoryFactory(DjangoModelFactory):
+
+    name = factory.sequence(lambda n: f"카테고리 ({n})")
+    slug = factory.Faker("slug")
+    order = factory.sequence(lambda n: n)
+
+    class Meta:
+        model = Category
+        django_get_or_create = ("name",)
+
+
+class PostFactory(DjangoModelFactory):
+
+    title = factory.Faker("sentence")
+    slug = factory.Faker("slug")
+    content = factory.Faker("paragraph", nb_sentences=30)
+    description = factory.lazy_attribute(lambda obj: obj.content[:140])
+    status = factory.fuzzy.FuzzyChoice(Post.Status)
+    is_page = False
+    category = factory.SubFactory(CategoryFactory)
+    thumbnail = factory.django.ImageField(color="red")
+
+    class Meta:
+        model = Post
+        django_get_or_create = ("slug",)
