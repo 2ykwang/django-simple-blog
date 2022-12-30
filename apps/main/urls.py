5afca46b4c47fae5cb90bb6febed39c1ce288c2b
Youngkwang Yang <immutable000@gmail.com>
2022-12-30 19:15:18 +0900

    Add ads view

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .apps import MainConfig as Config
-from .views import MainView
+from .views import MainView, AdsView
 
 app_name = Config.name
 
 urlpatterns = [
     path("", MainView.as_view(), name="main_index"),
+    path("ads.txt", AdsView, name="ads")
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.generic import View
+from django.http import HttpResponse
 
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
         return redirect(reverse("apps.posts:post_list"))
+
+
+def AdsView(request, *args, **kwargs):
+    return HttpResponse("google.com, pub-3049139078910304, DIRECT, f08c47fec0942fa0")
